d9caa79f7422d19aaea6765c276baf19370d6fbd
Carlos Leon <35080984+chinchay@users.noreply.github.com>
2022-07-08 11:29:17 -0400

    feat: Add required files for making a package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='GreenPy',
+    version='0.1',
+    description='Decimation of the Green function in graphene nanoribbons',
+    packages=['GreenPy'],
+    zip_safe=False
+    )
