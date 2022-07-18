f7dfe1e2e74900cff2a2e6e8e8f38f1291c892fe
Carlos Leon <35080984+chinchay@users.noreply.github.com>
2022-07-18 17:12:34 -0400

    docs: Add docstrings for Green class

--- src/green.py
+++ src/green.py
@@ -1,14 +1,29 @@
 import numpy as np
 from numpy.linalg import solve
 from numpy import matmul
-# import fortranLib
 
 eye2   = np.eye(2, dtype=complex)
 zeros2 = np.zeros(2, dtype=complex)
 
 class Green():
+    """Class to handle Green function, at a given energy E for a periodic device
+        - The system is considered as LEFT + CENTER + RIGHT parts
+        - Initialize the isolated Green function for the center device in the energy complex plane as `1 / E + i * eta` where `eta << 1`
+        - Interactions are given by the hopping matrices `t00`(inside the supercell), `t`(inter cell left), and `td`(inter cell right)
+        - Decimation or "dressing up" of the interacting Green function is calculated through iteratively use of the Dyson equation
+    """
+
     def __init__( self, t00=eye2, t=eye2, td=eye2, energy=-2.0, onsite_list=zeros2, eta=0.01 ):
-        # initialization
+        """Initialize the isolated Green function
+
+        Args:
+            - t00 : np.array type, cell self-interation
+            - t   : np.array type, CENTER-RIGHT interaction
+            - td  : np.array type, CENTER-LEFT interaction
+            - energy : float, 
+            - onsite_list : np.array type, on-site energies of each site on the cell
+            - eta : float, `<< 1`
+        """
         self.t00       = t00
         self.t         = t
         self.td        = td
@@ -21,20 +36,40 @@ class Green():
     #
     
     def __repr__(self) -> str:
+        """Provides information about the complex energy selected """
         return f"Green object with energy={round(self.energy, 3)}, eta={round(self.eta, 5)}"
         
     # @profile
     def renormalize(self, Z, Q):
+        """Calculates the "dressed-up" Green function 
+        
+        Specifically:
+        `GreenFunction_new = Inverse( Identity - Z ) * Q`
+        
+        Args:
+            - Z : numpy array, interaction information
+            - Q : numpy array, Green function
+
+        Returns:
+            - renormalized: numpy array, the new Green function of the dressed-up system
+        """
         size = Q.shape[0]
         ident = np.eye(size, dtype=complex)
         temp = ident - Z
         renormalized = solve(temp, Q)
-        # renormalized = myflib.solve(temp, Q, size)
         return renormalized
     #
 
     # @profile
-    def decimate(self):        
+    def decimate(self):
+        """Applies the Dyson equation iteratively
+
+        Args:
+            - None
+
+        Returns:
+            - GR, numpy array, the renormalized Green function throug the use of the `renormalize()` function
+        """ 
         # careful here, between .dot and matmul
         # https://stackoverflow.com/questions/34142485/difference-between-numpy-dot-and-python-3-5-matrix-multiplication#:~:text=matmul%20differs%20from%20dot%20in,if%20the%20matrices%20were%20elements.
         temp = matmul( self.greenFunc, self.t00 )
@@ -55,10 +90,19 @@ class Green():
     #
 
     def trace(self):
+        """Calculates the trace of the converged Green function to use later for electronic density curve vs energy"""
         return -np.trace( self.decimate().imag ) / (self.size * 3.14159)
 
     @staticmethod
     def get_density_OneLinearChain(energy):
+        """Get the electronic density of linear chain of sites
+        
+        Args:
+            - energy: float
+        
+        Returns:
+            - density: float
+        """
         eye1 = np.eye(1, dtype=complex)
         t00  = np.asarray( [0] )
         t    = eye1
@@ -72,6 +116,14 @@ class Green():
 
     @staticmethod
     def get_density_smallestZGNR(energy):
+        """Get the electronic density of a 2-ZGNR
+        
+        Args:
+            - energy: float
+        
+        Returns:
+            - density: float
+        """
         eye4 = np.eye(4, dtype=complex)
         t00 = [ [0, 1, 0, 0],
                 [1, 0, 1, 0],
@@ -97,6 +149,15 @@ class Green():
     
     @staticmethod
     def plot(energy_list, density_list):
+        """Script to display the electronic density of a system
+        
+        Args:
+            - energy_list: numpy array, 1-dimensional
+            - density_list: numpy array, 1-dimensional, same size as energy_list
+
+        Returns: 
+            - None
+        """
         import matplotlib.pyplot as plt
         min_energy = min(energy_list)
         max_energy = max(energy_list)
